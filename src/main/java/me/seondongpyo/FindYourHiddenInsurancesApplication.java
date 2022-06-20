d428a4c2a44a8c0a422b0a4131779173958ef892
seondongpyo <seondongpyo@gmail.com>
2022-06-20 23:17:38 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/me/seondongpyo/FindYourHiddenInsurancesApplication.java
@@ -0,0 +1,13 @@
+package me.seondongpyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindYourHiddenInsurancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindYourHiddenInsurancesApplication.class, args);
+    }
+
+}
