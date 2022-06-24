2aeb814455d1793320b26831b0a77b5b13b0fa5b
seondongpyo <seondongpyo@gmail.com>
2022-06-24 22:59:04 +0900

    feat: User 클래스에 `@Entity` 어노테이션 및 기본 생성자 추가

--- src/main/java/me/seondongpyo/domain/User.java
+++ src/main/java/me/seondongpyo/domain/User.java
@@ -1,12 +1,17 @@
 package me.seondongpyo.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class User {
 
 	@Id
