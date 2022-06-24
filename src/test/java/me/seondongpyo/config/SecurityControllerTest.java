1ed8a5f68516a432bef96151db6b0825542911a7
seondongpyo <seondongpyo@gmail.com>
2022-06-24 23:40:11 +0900

    test: '/manager' 하위 경로는 MANAGER와 ADMIN만 접근할 수 있다

--- src/test/java/me/seondongpyo/config/SecurityControllerTest.java
+++ src/test/java/me/seondongpyo/config/SecurityControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,5 +25,14 @@ class SecurityControllerTest {
         mvc.perform(get("/"))
             .andExpect(status().isOk())
             .andExpect(content().string("home"));
+    }
+
+    @DisplayName("'/manager' 하위 경로는 MANAGER와 ADMIN만 접근할 수 있다.")
+    @WithMockUser(roles = {"MANAGER", "ADMIN"})
+    @Test
+    void manager() throws Exception {
+        mvc.perform(get("/manager"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("manager or admin"));
     }
 }
