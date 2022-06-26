948c5e4cd4ddc63b2e0d2d760f54351612d2a019
seondongpyo <seondongpyo@gmail.com>
2022-06-27 00:51:47 +0900

    feat: PasswordEncoder를 스프링 빈으로 등록

--- src/main/java/me/seondongpyo/config/SecurityConfiguration.java
+++ src/main/java/me/seondongpyo/config/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package me.seondongpyo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,5 +26,10 @@ public class SecurityConfiguration {
             .permitAll();
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
