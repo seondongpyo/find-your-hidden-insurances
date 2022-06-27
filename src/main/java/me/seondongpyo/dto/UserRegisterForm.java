7c0551f8c5c1fdc666d01380743d7fd8f17920cc
seondongpyo <seondongpyo@gmail.com>
2022-06-27 23:16:14 +0900

    feat: 사용자 등록 시 Role을 선택하도록 추가

--- src/main/java/me/seondongpyo/dto/UserRegisterForm.java
+++ src/main/java/me/seondongpyo/dto/UserRegisterForm.java
@@ -12,8 +12,9 @@ public class UserRegisterForm {
     private String name;
     private String username;
     private String password;
+    private Role role;
 
     public User toUser() {
-        return new User(name, username, password, Role.USER);
+        return new User(name, username, password, role);
     }
 }
