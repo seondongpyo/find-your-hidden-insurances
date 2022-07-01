5b5aa7b39ca375789f33e892c2f75a540993d1f4
seondongpyo <seondongpyo@gmail.com>
2022-07-01 21:09:12 +0900

    feat: 사용자 조회 API 추가

--- src/main/java/me/seondongpyo/controller/UserController.java
+++ src/main/java/me/seondongpyo/controller/UserController.java
@@ -1,13 +1,18 @@
 package me.seondongpyo.controller;
 
-import lombok.RequiredArgsConstructor;
-import me.seondongpyo.application.UserService;
-import me.seondongpyo.dto.UserRegisterForm;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.RequiredArgsConstructor;
+import me.seondongpyo.application.UserService;
+import me.seondongpyo.domain.User;
+import me.seondongpyo.dto.UserRegisterForm;
+import me.seondongpyo.dto.UserResponseDTO;
+import me.seondongpyo.security.AuthUser;
 
 @RequiredArgsConstructor
 @Controller
@@ -25,5 +30,13 @@ public class UserController {
     public String register(@ModelAttribute("form") UserRegisterForm form) {
         userService.create(form.toUser());
         return "redirect:/login";
+    }
+
+    @GetMapping("/detail")
+    public String detail(@AuthenticationPrincipal AuthUser authUser,
+                         Model model) {
+        User user = userService.findById(authUser.getId());
+        model.addAttribute("user", new UserResponseDTO(user));
+        return "user/detail";
     }
 }
--- src/main/java/me/seondongpyo/security/AuthUser.java
+++ src/main/java/me/seondongpyo/security/AuthUser.java
@@ -1,22 +1,58 @@
 package me.seondongpyo.security;
 
-import lombok.Getter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-
 import java.util.Collection;
 import java.util.List;
 
-@Getter
-public class AuthUser extends User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-    public AuthUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, authorities);
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.seondongpyo.domain.User;
+
+@RequiredArgsConstructor
+@Getter
+public class AuthUser implements UserDetails {
+
+    private final User user;
+
+    public Long getId() {
+        return user.getId();
     }
 
-    public AuthUser(me.seondongpyo.domain.User user) {
-        this(user.getUsername(), user.getPassword(),
-            List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole())));
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
