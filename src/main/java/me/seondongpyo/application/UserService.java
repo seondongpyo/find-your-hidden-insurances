78dbb29875e5574dd39da5141a59604d9a01eac4
seondongpyo <seondongpyo@gmail.com>
2022-07-01 21:35:05 +0900

    feat: 사용자 정보 수정 기능 추가

--- src/main/java/me/seondongpyo/application/UserService.java
+++ src/main/java/me/seondongpyo/application/UserService.java
@@ -2,15 +2,22 @@ package me.seondongpyo.application;
 
 import java.util.Optional;
 
-import lombok.RequiredArgsConstructor;
-import me.seondongpyo.domain.User;
-import me.seondongpyo.domain.UserRepository;
-import me.seondongpyo.exception.DuplicateUsernameException;
-import me.seondongpyo.exception.UserNotFoundException;
+import javax.annotation.PostConstruct;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import me.seondongpyo.domain.Role;
+import me.seondongpyo.domain.User;
+import me.seondongpyo.domain.UserRepository;
+import me.seondongpyo.dto.UserUpdateForm;
+import me.seondongpyo.exception.DuplicateUsernameException;
+import me.seondongpyo.exception.UserNotFoundException;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class UserService {
 
@@ -26,8 +33,21 @@ public class UserService {
 		return userRepository.save(user);
 	}
 
+	@Transactional(readOnly = true)
 	public User findById(Long id) {
 		return userRepository.findById(id)
 			.orElseThrow(() -> new UserNotFoundException(id));
+	}
+
+	public void update(Long id, UserUpdateForm updateParam) {
+		User user = findById(id);
+		user.update(updateParam);
+	}
+
+	@PostConstruct
+	private void init() {
+		userRepository.save(new User("어드민", "admin", passwordEncoder.encode("1234"), Role.ADMIN));
+		userRepository.save(new User("매니저", "manager", passwordEncoder.encode("1234"), Role.MANAGER));
+		userRepository.save(new User("유저", "user", passwordEncoder.encode("1234"), Role.USER));
 	}
 }
--- src/main/java/me/seondongpyo/controller/UserController.java
+++ src/main/java/me/seondongpyo/controller/UserController.java
@@ -1,5 +1,6 @@
 package me.seondongpyo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,7 @@ import me.seondongpyo.application.UserService;
 import me.seondongpyo.domain.User;
 import me.seondongpyo.dto.UserRegisterForm;
 import me.seondongpyo.dto.UserResponseDTO;
+import me.seondongpyo.dto.UserUpdateForm;
 import me.seondongpyo.security.AuthUser;
 
 @RequiredArgsConstructor
@@ -20,25 +22,43 @@ import me.seondongpyo.security.AuthUser;
 @Controller
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    @GetMapping("/register")
-    public String register(Model model) {
-        model.addAttribute("form", new UserRegisterForm());
-        return "user/register";
-    }
+	@GetMapping("/register")
+	public String register(Model model) {
+		model.addAttribute("form", new UserRegisterForm());
+		return "user/register";
+	}
 
-    @PostMapping("/register")
-    public String register(@ModelAttribute("form") UserRegisterForm form) {
-        userService.create(form.toUser());
-        return "redirect:/login";
-    }
+	@PostMapping("/register")
+	public String register(@ModelAttribute("form") UserRegisterForm form) {
+		userService.create(form.toUser());
+		return "redirect:/login";
+	}
 
-    @GetMapping("/detail")
-    public String detail(@AuthenticationPrincipal AuthUser authUser,
-                         Model model) {
-        User user = userService.findById(authUser.getId());
-        model.addAttribute("user", new UserResponseDTO(user));
-        return "user/detail";
-    }
+	@GetMapping("/detail")
+	@PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+	public String detail(@AuthenticationPrincipal AuthUser authUser,
+						 Model model) {
+		User user = userService.findById(authUser.getId());
+		model.addAttribute("user", new UserResponseDTO(user));
+		return "user/detail";
+	}
+
+	@GetMapping("/edit")
+	@PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+	public String edit(@AuthenticationPrincipal AuthUser authUser,
+					   Model model) {
+		User user = userService.findById(authUser.getId());
+		model.addAttribute("user", new UserResponseDTO(user));
+		return "user/edit";
+	}
+
+	@PostMapping("/edit")
+	@PreAuthorize("isAuthenticated() and hasAuthority('USER')")
+	public String edit(@AuthenticationPrincipal AuthUser authUser,
+					   @ModelAttribute UserUpdateForm form) {
+		userService.update(authUser.getId(), form);
+		return "redirect:/user/detail";
+	}
 }
