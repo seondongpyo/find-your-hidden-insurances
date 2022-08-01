d2a7fe3830eff42272ac395d3cc3be7e8b2840f0
seondongpyo <seondongpyo@gmail.com>
2022-08-01 22:53:33 +0900

    fix: 환경설정 변수 오타 수정

--- src/main/java/me/seondongpyo/application/MyDataHubApiService.java
+++ src/main/java/me/seondongpyo/application/MyDataHubApiService.java
@@ -2,18 +2,20 @@ package me.seondongpyo.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
+@PropertySource("classpath:application-dev.properties")
 @Service
 public class MyDataHubApiService {
 
-    @Value("${mydatahub.api.insure-money-inquiry-uri}")
+    @Value("${mydatahub.api.insure-money-inquiry-url}")
     private String insureMoneyInquiryURI;
 
     @Value("${mydatahub.api.captcha-sms-uri}")
     private String captchaSmsURI;
 
-    @Value("${mydatahub.token}")
+    @Value("${mydatahub.api.token}")
     private String token;
 }
